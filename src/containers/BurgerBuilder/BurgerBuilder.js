b5ecb787e24450dc75b7377fadfea045703adb00
Shreyas Kalyanaraman <shreyas.kalyanaraman@gmail.com>
2019-04-21 06:32:18 -0400

    V1.0 - Basic Burger
    This commit containts basic burger layout with hardcoded values of ingredients. No dynamic contant yet!

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+
+import Aux from '../../hoc/Auxillary';
+
+import Burger from '../../components/Burger/Burger'
+
+class BurgerBuilder extends Component{
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 2,
+            cheese: 3,
+            meat: 1,
+        }
+    }
+
+    render () {
+        return (
+            <Aux>
+                <Burger ingredients={this.state.ingredients} />
+                <div>Build Controls</div>
+            </Aux>
+        );
+    };
+}
+
+export default BurgerBuilder;

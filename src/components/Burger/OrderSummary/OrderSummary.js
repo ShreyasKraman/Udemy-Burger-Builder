f009a592efce74b97c8a68646c0fb9955b7e7e86
Shreyas Kalyanaraman <shreyas.kalyanaraman@gmail.com>
2019-05-16 13:38:19 -0400

    Minor update

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -5,15 +5,15 @@ import Aux from '../../../hoc/Auxillary/Auxillary';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 
-const Ordersummary = () => {
+const Ordersummary = (props) => {
 
-        const ingredients = Object.keys(this.props.ingredients)
+        const ingredients = Object.keys(props.ingredients)
                 .map(igKey => {
                     return <li key={igKey}>
                                 <span style={{textTransform:'capitalize'}}>
                                     {igKey}
                                 </span>
-                                :{this.props.ingredients[igKey]}
+                                :{props.ingredients[igKey]}
                             </li>;
                 });     
     
@@ -23,20 +23,20 @@ const Ordersummary = () => {
                 <h3>Your Order Summary</h3>
                 <p>Following are ingredients in your delicious burger</p>
                 <ul>{ingredients}</ul>
-                <h3>Total price: $ {this.props.price.toFixed(2)}</h3>
+                <h3>Total price: $ {props.price.toFixed(2)}</h3>
                 <p>Continue to checkout?</p>
                 <Grid container spacing={24}>
                     <Grid item xs={4}>
                         <Button 
                             variant="contained" 
                             color="secondary" 
-                            onClick={this.props.close}>Cancel</Button>
+                            onClick={props.close}>Cancel</Button>
                     </Grid>
                     <Grid item xs={4}>
                         <Button
                             varaint="contained"
                             color="primary"
-                            onClick={this.props.continue}>Continue</Button>
+                            onClick={props.continue}>Continue</Button>
                     </Grid>
                 </Grid>
             </Aux>
